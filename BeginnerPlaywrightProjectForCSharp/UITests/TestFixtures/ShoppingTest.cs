7517eb1091c28d13c2c6338e9a491edcfaddee5e
codingWithUsername <sharon_dt@hotmail.com>
2024-12-24 09:16:55 +0100

    Added a new test

---
+++ BeginnerPlaywrightProjectForCSharp/UITests/TestFixtures/ShoppingTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeginnerPlaywrightProjectForCSharp.UITests.PageObjectModels;
+
+namespace BeginnerPlaywrightProjectForCSharp.UITests.TestFixtures
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    internal class ShoppingTest : Setup
+    {
+        [Test]
+        public async Task FilterProductResults()
+        {
+            ShoppingPage shoppingPage = new ShoppingPage(Page);
+            string usernameQL = "standard_user";
+            string passwordQL = "secret_sauce";
+            await Page.GotoAsync("https://www.saucedemo.com/");
+            await shoppingPage.QuickLogin(usernameQL, passwordQL);
+            await shoppingPage.FilterProductResults();
+        }
+    }
+}
